f29409d394546a4d9be5677e733dbed2f9a60ac0
ayuanjm <2459087172@qq.com>
2019-10-11 17:42:41 +0800

    初始化项目

---
+++ middleware-redis/src/main/java/com/yuan/middleware/MiddlewareRedisApplication.java
@@ -0,0 +1,16 @@
+package com.yuan.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuan
+ */
+@SpringBootApplication
+public class MiddlewareRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareRedisApplication.class, args);
+    }
+
+}
