4af987e5695ea5118967f97a19e4287a2fb3bec4
ayuanjm <2459087172@qq.com>
2019-10-19 22:10:21 +0800

    路由模式-交换机

--- middleware-rabbitmq/src/main/java/com/yuan/middleware/demo/product/Product.java
+++ middleware-rabbitmq/src/main/java/com/yuan/middleware/demo/product/Product.java
@@ -1,33 +1,35 @@
-package com.yuan.middleware.publish.product;
+package com.yuan.middleware.demo.product;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 import static com.yuan.middleware.util.RabbitMqConnection.getConnection;
 
 /**
- * publish/subscribe 发布订阅模式
- * 一个生产者多个消费者，每一个消费者都有自己的队列，生产者没有直接把消息发送到队列，
- * 而是发送到了交换机exchange，每个队列都要绑定到交换机上，生产者发送的消息经过交换机到达队列，
- * 就能实现一个消息被多个消费者消费。
- * ​消息始终都是先发送到交换机，由交换机经过路由传送给队列，消费者再从队列中获取消息的！
- *
  * @author yuan
  */
 @Slf4j
 public class Product {
     public static void main(String[] args) throws IOException, TimeoutException {
+        sendMsg();
+
+    }
+
+    private static void sendMsg() throws IOException, TimeoutException {
         Connection connection = getConnection();
         // 建立通道
         Channel channel = connection.createChannel();
-        String queueNameA = "test_queue_fanout_a";
-        String queueNameB = "test_queue_fanout_b";
+        String queueNameA = "test_ack_queue_a";
+        String queueNameB = "test_ack_queue_b";
 
-        String exchangeName = "test_exchange_fanout";
+        String exchangeName = "test_ack_exchange";
         // 声明队列
         /**
          * @params 参数列表：
@@ -43,15 +45,12 @@ public class Product {
         // 声明交换机
         /**
          * String exchange, 交换机名称
-         * String type,  交换机类型，需要根据情况指定类型
-         *（FANOUT）扇形交换机，（DIRECT）直连交换机，（TOPIC） 主题交换机，也叫通配符交换机，（HEADER）首部交换机
-         *
-         * 扇形交换机fanout:不处理路由键(routingKey失效)，会发送到所有与之绑定的队列。
-         * 就算发送绑定交换机时指定了routingKey，发送时指定了routingKey,也会送到所有与之绑定的队列，路由失效。
-         *
+         * String type,  交换机类型
          * boolean durable  是否持久化
          */
-        channel.exchangeDeclare(exchangeName, "fanout", true, false, null);
+        channel.exchangeDeclare(exchangeName, "topic", true, false, null);
+
+        String routingKey = "ack.save";
 
         // 交换机跟队列绑定,或者说创建了一个binding，
         // 其实就是交换机跟队列的一个绑定关系
@@ -60,14 +59,21 @@ public class Product {
          * String exchange,  交换机名称
          * String routingKey 路由key,路由模式下，我们指定这个key值
          */
-        channel.queueBind(queueNameA, exchangeName, "1");
-        channel.queueBind(queueNameB, exchangeName, "2");
+        channel.queueBind(queueNameA, exchangeName, routingKey);
+        channel.queueBind(queueNameB, exchangeName, "routingKey");
 
 
         for (int i = 0; i < 5; i++) {
-            String msg = "Hello RabbitMQ Message " + i;
+            Map<String, Object> headers = new HashMap<>(16);
+            headers.put("num", i);
+            AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+                    .deliveryMode(2)
+                    .contentEncoding("UTF-8")
+                    .headers(headers)
+                    .build();
+            String msg = "Hello RabbitMQ ACK Message " + i;
             log.info("生产端发送：{}", msg);
-            channel.basicPublish(exchangeName, "3", null, msg.getBytes());
+            channel.basicPublish(exchangeName, routingKey, true, properties, msg.getBytes());
         }
         connection.close();
     }
