976fdb75deb9a595061617e29887544edbeeb1b8
ayuanjm <2459087172@qq.com>
2019-11-19 23:23:01 +0800

    自增原理与操作栈

---
+++ middleware-demo/src/main/java/com/yuan/middleware/MiddlewareDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yuan.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareDemoApplication.class, args);
+    }
+
+}
