6e2974e7d0d0f007dca45ecdb64e241bbf4a577d
ayuanjm <2459087172@qq.com>
2019-11-21 00:51:04 +0800

    单例模式

---
+++ middleware-demo/src/main/java/com/yuan/middleware/base/design/single/Singleton1.java
@@ -0,0 +1,23 @@
+package com.yuan.middleware.base.design.single;
+
+import java.util.Date;
+
+/**
+ * 饿汉式 在类初始化时直接创建对象，不存在线程安全问题，不管你是否需要这个对象都会创建
+ * <p>
+ * 1. 构造器私有化
+ * 2. 自行创建，并且用静态变量保存
+ * 3. 向外提供这个实例
+ * 4. 强调这是一个单例，我们可以用final修饰，不能改变引用，可以改变属性。
+ *
+ * @author yuan
+ */
+public class Singleton1 {
+    public static final Singleton1 INSTANCE = new Singleton1();
+
+    private static Date date = new Date();
+
+    private Singleton1() {
+
+    }
+}
---
+++ middleware-demo/src/main/java/com/yuan/middleware/base/design/single/Singleton2.java
@@ -0,0 +1,21 @@
+package com.yuan.middleware.base.design.single;
+
+/**
+ * 饿汉式
+ * <p>
+ * 枚举类型：表示该类型的对象是有限的几个
+ * 我们可以限定成一个，就成了单例
+ * <p>
+ * public enum Size{ SMALL, MEDIUM, LARGE, EXTRA_LARGE };
+ * 实际上，这个声明定义的类型是一个类，它刚好有四个实例，在此尽量不要构造新对象。
+ * https://www.cnblogs.com/liaojie970/p/6474733.html
+ *
+ * @author yuan
+ */
+public enum Singleton2 {
+    /**
+     * 相当于 Singleton2 INSTANCE = new Singleton2()
+     * 反编译可以看到这个类的构造函数是私有的: private Singleton2() {}
+     */
+    INSTANCE
+}
