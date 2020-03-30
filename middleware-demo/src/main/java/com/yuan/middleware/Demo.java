b0c74a35911f614edcdd4d6468132ac7179b74f8
ayuanjm <2459087172@qq.com>
2020-03-30 14:29:40 +0800

    测试mac 配置多个git

--- middleware-demo/src/main/java/com/yuan/middleware/Demo.java
+++ middleware-demo/src/main/java/com/yuan/middleware/Demo.java
@@ -1,21 +1,40 @@
 package com.yuan.middleware;
 
-import org.springframework.beans.factory.InitializingBean;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yjm
- * @date 2020/3/11 8:53 下午
+ * @date 2020/3/26 6:31 下午
  */
-public class Demo implements InitializingBean {
+public class Demo {
     public static void main(String[] args) {
-        byte[] a = new byte[1024];
-        for (;;){
-            a = new byte[1024];
-        }
+        Demo demo = new Demo();
+        new Thread(()->{
+            try {
+                demo.m1();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(()->{
+            try {
+                demo.m2();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public synchronized void m1() throws InterruptedException {
+        System.out.println(1);
+        TimeUnit.SECONDS.sleep(10);
+        System.out.println(1);
+    }
 
+    public synchronized void m2() throws InterruptedException {
+        System.out.println(2);
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(2);
     }
 }
