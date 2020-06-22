cc4fa00773e7cbca1d2e3a1285c0ffe5968c6749
ayuanjm <2459087172@qq.com>
2020-06-22 17:18:34 +0800

    删除排序链表中的重复元素

--- algorithm/leetcode/editor/cn/[83]删除排序链表中的重复元素.java
+++ algorithm/leetcode/editor/cn/[83]删除排序链表中的重复元素.java
@@ -14,17 +14,31 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
  * }
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                //11233最终跳出循环时cur.next取到了必定是连续相等元素的最后一个值(1,3)
+                while (cur.next != null && cur.next.next != null && cur.next.val == cur.next.next.val) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
