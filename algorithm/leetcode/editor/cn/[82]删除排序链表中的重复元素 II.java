6f5c7f8c26032cc85d2aab21673a0ef3a232602b
ayuanjm <2459087172@qq.com>
2020-06-22 16:40:23 +0800

    删除排序链表中的重复元素II

--- algorithm/leetcode/editor/cn/[82]删除排序链表中的重复元素 II.java
+++ algorithm/leetcode/editor/cn/[82]删除排序链表中的重复元素 II.java
@@ -30,6 +30,33 @@ import java.util.Set;
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        //创建一个虚拟节点，该节点的next指向头节点也就是第一个节点，类似aqs的头节点
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        //创建一个哨兵节点cur用来遍历,cur.next总是指向第一个元素
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            //如果连续元素相等
+            if (cur.next.val == cur.next.next.val) {
+                ListNode temp = cur.next;
+                //11123：最终temp会等于相等最后一个元素值(1)
+                while (temp != null && temp.next != null && temp.next.val == temp.val) {
+                    temp = temp.next;
+                }
+                //当前节点的next指向去除相等值后的第一个值(2),等同于将相等值从链表移除，
+                cur.next = temp.next;
+            } else {
+                //哨兵cur引用指向最新不等元素(2)
+                cur = cur.next;
+                //对于引用指向问题总感觉不太好理解，比如tail指向最后一个元素，新插入一个元素，tail.next = new; tail = new;
+                //tail相当于一个哨兵，总是指向最后一个元素，tail只是一个引用，指向堆内存的实例数据，tail.next改变会改变链表的最后一个元素的指向
+                //tail = new 不会改变链表的元素，只是tail指向了新的堆内存，但是不影响原来的tail指向的堆内存。
+            }
+        }
+        return dummy.next;
+    }
+
+    private ListNode getListNode(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
