e62d12b4af6510c3436592dfbbcc45fb51a81e61
ayuanjm <2459087172@qq.com>
2020-06-26 10:51:28 +0800

    移除链表元素

--- algorithm/leetcode/editor/cn/[203]移除链表元素.java
+++ algorithm/leetcode/editor/cn/[203]移除链表元素.java
@@ -9,17 +9,48 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
  * }
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur != null && cur.next != null) {
+            if (cur.next.val == val) {
+                //最后得到得cur.next一定是不等于val的
+                while (cur != null && cur.next != null && cur.next.val == val) {
+                    cur.next = cur.next.next;
+                }
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
 
+    //        return getListNode(head, val);
+    private ListNode getListNode(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        //有点类似双指针 pre cur
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+            } else {
+                pre = pre.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
