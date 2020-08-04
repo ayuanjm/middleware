111a442e27abdb6686adbbe13c8ade62d5658fb2
ayuanjm <2459087172@qq.com>
2020-08-04 18:22:55 +0800

    添加数据库连接池配置

--- middleware-mysql/src/main/java/com/yuan/middleware/separate/datasource/MyBatisConfig.java
+++ middleware-mysql/src/main/java/com/yuan/middleware/separate/datasource/MyBatisConfig.java
@@ -12,10 +12,13 @@ import javax.annotation.Resource;
 /**
  * Mybatis配置
  * <p>
+ * SqlSessionUtils.class
+ * SqlSessionHolder holder = (SqlSessionHolder) TransactionSynchronizationManager.getResource(sessionFactory);
+ * SpringManagedTransaction.class
  * ConnectionHolder conHolder = (ConnectionHolder) TransactionSynchronizationManager.getResource(dataSource);
  * 执行sql时会通过这个语句去获取数据库连接，如果有事务，那么在创建了事务后会从数据库连接池拿一个数据库连接，
  * 把数据库连接和当前事务线程绑定放入threadLocal中，之后执行sql语句时就会从threadLocal中获取,就不会再拿新的数据库连接。
- * key是resources，value是map，map:{routingDataSource:connectionHolder,DefaultSessionFactory,SqlSessionHolder}
+ * key是resources，value是map，map:{routingDataSource:connectionHolder,DefaultSessionFactory:SqlSessionHolder}
  * ThreadLocal<Map<Object, Object>> resources = new NamedThreadLocal<>("Transactional resources");
  * 如果没有事务或者有事务但是routingDataSource被改变，这样去threadLocal中就拿不到数据库连接，机会去创建新的数据库连接。
  *
