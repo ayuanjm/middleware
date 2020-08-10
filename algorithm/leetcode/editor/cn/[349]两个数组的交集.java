531acf0a6095e5d2be00ee82a77b7993a60ef63f
ayuanjm <2459087172@qq.com>
2020-08-10 18:05:22 +0800

    两个数组的交集

--- algorithm/leetcode/editor/cn/[349]两个数组的交集.java
+++ algorithm/leetcode/editor/cn/[349]两个数组的交集.java
@@ -28,6 +28,37 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        int index1 = 0;
+        int index2 = 0;
+        int index3 = 0;
+        int[] temp = new int[Math.min(length1, length2)];
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                //相等
+                int index = index3 - 1;
+                if (index >= 0 && temp[index] != nums1[index1]) {
+                    temp[index3++] = nums1[index1];
+                }
+                if (index == -1) {
+                    temp[0] = nums1[index1];
+                    index3++;
+                }
+                index1++;
+                index2++;
+            }
+        }
+        return Arrays.copyOf(temp, index3);
+    }
+
+    private int[] getInts(int[] nums1, int[] nums2) {
         Map map = new HashMap<Integer, Integer>();
         for (int a : nums1) {
             map.put(a, 1);
@@ -40,7 +71,7 @@ class Solution {
                 map.remove(nums2[i]);
             }
         }
-        return Arrays.copyOf(temp,k);
+        return Arrays.copyOf(temp, k);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
