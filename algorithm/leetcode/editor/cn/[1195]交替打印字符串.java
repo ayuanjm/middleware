fd8c3ab203b061a03ea446bae1e8567ebdf226d4
ayuanjm <2459087172@qq.com>
2020-09-02 10:23:09 +0800

    交替打印字符串

---
+++ algorithm/leetcode/editor/cn/[1195]交替打印字符串.java
@@ -0,0 +1,101 @@
+//编写一个可以从 1 到 n 输出代表这个数字的字符串的程序，但是： 
+//
+// 
+// 如果这个数字可以被 3 整除，输出 "fizz"。 
+// 如果这个数字可以被 5 整除，输出 "buzz"。 
+// 如果这个数字可以同时被 3 和 5 整除，输出 "fizzbuzz"。 
+// 
+//
+// 例如，当 n = 15，输出： 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14
+//, fizzbuzz。 
+//
+// 假设有这么一个类： 
+//
+// class FizzBuzz {
+//  public FizzBuzz(int n) { ... }               // constructor
+//  public void fizz(printFizz) { ... }          // only output "fizz"
+//  public void buzz(printBuzz) { ... }          // only output "buzz"
+//  public void fizzbuzz(printFizzBuzz) { ... }  // only output "fizzbuzz"
+//  public void number(printNumber) { ... }      // only output the numbers
+//} 
+//
+// 请你实现一个有四个线程的多线程版 FizzBuzz， 同一个 FizzBuzz 实例会被如下四个线程使用： 
+//
+// 
+// 线程A将调用 fizz() 来判断是否能被 3 整除，如果可以，则输出 fizz。 
+// 线程B将调用 buzz() 来判断是否能被 5 整除，如果可以，则输出 buzz。 
+// 线程C将调用 fizzbuzz() 来判断是否同时能被 3 和 5 整除，如果可以，则输出 fizzbuzz。 
+// 线程D将调用 number() 来实现输出既不能被 3 整除也不能被 5 整除的数字。 
+// 
+// 👍 29 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class FizzBuzz {
+    private int n;
+
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(4);
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                printFizz.run();
+            }
+            //每一次循环都需要等待，这样每个i在四个线程中只有一个线程符合要求会输出，大家都等待了一次刚好是4，又可以进入下一次循环。
+            //等待不能放在if里面，否则不能保证输出的顺序。
+            try {
+                cyclicBarrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 != 0 && i % 5 == 0) {
+                printBuzz.run();
+            }
+            try {
+                cyclicBarrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                printFizzBuzz.run();
+            }
+            try {
+                cyclicBarrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                printNumber.accept(i);
+            }
+            try {
+                cyclicBarrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
