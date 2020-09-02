796573ab33de3cbab27c9fdd6c59a46e047b5f38
ayuanjm <2459087172@qq.com>
2020-09-02 16:31:59 +0800

    从不订购的客户

---
+++ algorithm/leetcode/editor/cn/[183]从不订购的客户.java
@@ -0,0 +1,39 @@
+//某网站包含两个表，Customers 表和 Orders 表。编写一个 SQL 查询，找出所有从不订购任何东西的客户。 
+//
+// Customers 表： 
+//
+// +----+-------+
+//| Id | Name  |
+//+----+-------+
+//| 1  | Joe   |
+//| 2  | Henry |
+//| 3  | Sam   |
+//| 4  | Max   |
+//+----+-------+
+// 
+//
+// Orders 表： 
+//
+// +----+------------+
+//| Id | CustomerId |
+//+----+------------+
+//| 1  | 3          |
+//| 2  | 1          |
+//+----+------------+
+// 
+//
+// 例如给定上述表格，你的查询应返回： 
+//
+// +-----------+
+//| Customers |
+//+-----------+
+//| Henry     |
+//| Max       |
+//+-----------+
+// 
+// 👍 169 👎 0
+
+
+//There is no code of Java type for this problem
+select Name as 'Customers' from Customers where id not in (select CustomerId from Orders);
+select Name as 'Customers' from Customers where not exists (select id from Orders where CustomerId = Customers.id);
