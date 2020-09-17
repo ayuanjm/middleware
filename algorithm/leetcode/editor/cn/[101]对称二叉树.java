38a0ce66dce19225ec7e6106ed3c2ef7986f50c9
ayuanjm <2459087172@qq.com>
2020-09-17 11:18:23 +0800

    对称二叉树

---
+++ algorithm/leetcode/editor/cn/[101]对称二叉树.java
@@ -0,0 +1,93 @@
+//给定一个二叉树，检查它是否是镜像对称的。 
+//
+// 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+// 
+//
+// 
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索 
+// 👍 953 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    /**
+     * 我们用递归的方法实现了对称性的判断，那么如何用迭代的方法实现呢？首先我们引入一个队列，这是把递归程序改写成迭代程序的常用方法。
+     * 初始化时我们把根节点入队两次。每次提取两个结点并比较它们的值（队列中每两个连续的结点应该是相等的，而且它们的子树互为镜像），
+     * 然后将两个结点的左右子结点按相反的顺序插入队列中。当队列为空时，或者我们检测到树不对称（即从队列中取出两个不相等的连续结点）时，该算法结束。
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    public boolean check(TreeNode left, TreeNode right) {
+//        if (left == null && right == null) {
+//            return true;
+//        }
+//        if (left == null || right == null) {
+//            return false;
+//        }
+//        return left.val == right.val && check(left.right, right.left) && check(left.left, right.right);
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(left);
+        queue.add(right);
+        while (!queue.isEmpty()) {
+            TreeNode q = queue.removeFirst();
+            TreeNode p = queue.removeFirst();
+            if (q == null && p == null) {
+                continue;
+            }
+            if ((q == null || p == null) || q.val != p.val) {
+                return false;
+            }
+
+            queue.add(q.left);
+            queue.add(p.right);
+
+            queue.add(q.right);
+            queue.add(p.left);
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
