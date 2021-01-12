99206e0d88cc8c58ee292a1258c8a9157af237af
ayuanjm <2459087172@qq.com>
2021-01-12 19:51:09 +0800

    合并排序的数组

---
+++ algorithm/leetcode/editor/cn/[面试题 10.01]合并排序的数组.java
@@ -0,0 +1,44 @@
+//给定两个排序后的数组 A 和 B，其中 A 的末端有足够的缓冲空间容纳 B。 编写一个方法，将 B 合并入 A 并排序。 
+//
+// 初始化 A 和 B 的元素数量分别为 m 和 n。 
+//
+// 示例: 
+//
+// 输入:
+//A = [1,2,3,0,0,0], m = 3
+//B = [2,5,6],       n = 3
+//
+//输出: [1,2,2,3,5,6] 
+//
+// 说明: 
+//
+// 
+// A.length == n + m 
+// 
+// Related Topics 数组 双指针 
+// 👍 94 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void merge(int[] A, int m, int[] B, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = A.length - 1;
+        while (i >= 0 && j >= 0) {
+            if (A[i] > B[j]) {
+                A[k--] = A[i--];
+            } else {
+                A[k--] = B[j--];
+            }
+        }
+
+        while (i >= 0) {
+            A[k--] = A[i--];
+        }
+        while (j >= 0) {
+            A[k--] = B[j--];
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
