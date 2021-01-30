0709925ad84d9f45e79cd81c3950f64344ba3192
ayuanjm <2459087172@qq.com>
2021-01-30 23:33:15 +0800

    第一个只出现一次的字符

---
+++ algorithm/leetcode/editor/cn/[剑指 Offer 50]第一个只出现一次的字符.java
@@ -0,0 +1,37 @@
+//在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。 
+//
+// 示例: 
+//
+// s = "abaccdeff"
+//返回 "b"
+//
+//s = "" 
+//返回 " "
+// 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= s 的长度 <= 50000 
+// Related Topics 哈希表 
+// 👍 70 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public char firstUniqChar(String s) {
+        int[] arr = new int[26];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[s.charAt(i) - 'a'] == 1) {
+                return s.charAt(i);
+            }
+        }
+        return ' ';
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
